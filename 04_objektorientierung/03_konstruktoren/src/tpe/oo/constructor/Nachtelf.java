6a626ba60b8252564a5e2180843802b0d92502c0
b.collins <b.collins@bw-lp>
2016-10-24 12:00:15 +0200

    imlemented constructors (one and two apammeters) for nachtelf and untoter

--- 04_objektorientierung/03_konstruktoren/src/tpe/oo/constructor/Nachtelf.java
+++ 04_objektorientierung/03_konstruktoren/src/tpe/oo/constructor/Nachtelf.java
@@ -3,7 +3,7 @@ package tpe.oo.constructor;
 /**
  * Nachtelf aus der Allianz.
  */
-public class Nachtelf /* extends Figur */ {
+public class Nachtelf extends Wesen {
 
     /** Standardmäßige Stärke der Fähigkeit Naturwiderstand. */
     public static final int STANDARD_NATURWIDERSTAND = 5;
@@ -12,7 +12,15 @@ public class Nachtelf /* extends Figur */ {
     private int naturwiderstand;
     
     // Konstruktoren fehlen. Bitte hier einfügen.
+ public   Nachtelf(String name,int x){
+        super(name);
+        naturwiderstand=x;
+    }
+ public   Nachtelf(String name){
+        super(name);
+        naturwiderstand=STANDARD_NATURWIDERSTAND;
     
+    }
     /**
      * Stärke des Naturwiderstandes.
      * 
