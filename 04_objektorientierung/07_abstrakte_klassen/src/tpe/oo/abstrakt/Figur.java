4351dbe960d305b8055e0517a88419a56b35c010
b.collins <b.collins@bw-lp>
2016-10-31 10:57:49 +0100

    we made an abstract class Figur. whit an abstract method flaeche witch returns the area of the object as an double.

---
+++ 04_objektorientierung/07_abstrakte_klassen/src/tpe/oo/abstrakt/Figur.java
@@ -0,0 +1,18 @@
+package tpe.oo.abstrakt;
+/**
+ * This class implements the basic concepts of an geometric object.
+ *
+ * @author b.collins
+ *
+ */
+public abstract class Figur {
+    /**
+     * the abstract method implements the
+     * computation of the area of an geometric object.
+     *
+     * @return
+     * @return returns the area of the object.
+     */
+    abstract double flaeche();
+
+}
