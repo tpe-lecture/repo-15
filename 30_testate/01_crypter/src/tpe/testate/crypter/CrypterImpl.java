1e9b374e4f608974b8035160d5bf4007cd90c6c1
b.collins <b.collins@bw-lp>
2016-12-02 13:05:22 +0100

    Rewrote the class CrypterImpl in an shorter Version and added javaDoc

--- 30_testate/01_crypter/src/tpe/testate/crypter/CrypterImpl.java
+++ 30_testate/01_crypter/src/tpe/testate/crypter/CrypterImpl.java
@@ -1,6 +1,21 @@
 package tpe.testate.crypter;
 
+/**
+ * Die Klasse implementiert das Interface Crypter. Texte werden mit der
+ * speziellen TPE-Verschlüsselung verschlüsselt. Die Verschlüsselung
+ * funktioniert so, dass bestimmte Zeichen durch andere ersetzt werden.
+ *
+ * Hierbei gilt die folgende Übersetzungstabelle:
+ *
+ * <code> e -> 3 3 -> e l -> 1 1 -> l o -> 0 0 -> o a -> 4 4 -> a t -> 7 7 -> t
+ * 
+ * @author b.collins
+ *
+ */
 public class CrypterImpl implements Crypter {
+	private char[] arrayHin = "abcdefghijklmnopqrstuvwxyz0123456789 ".toCharArray();
+	private char[] arrayZurueck = "4bcd3fghijk1mn0pqrs7uvwxyzol2ea56t89 ".toCharArray();
+
 	/**
 	 * Verschlüsselt den gegebenen Text. Enthält die Eingabe Großbuchstaben, so
 	 * werden diese in Kleinbuchstaben umgewandelt. Ungültige Zeichen werden
@@ -12,126 +27,16 @@ public class CrypterImpl implements Crypter {
 	 */
 	public String encrypt(String input) {
 		input = input.toLowerCase();
-		char[] outputArray = input.toCharArray();
-		for (int i = 0; i < input.length(); i++) {
-			char x = input.charAt(i);
-			switch (x) {
-			case 'e':
-				outputArray[i] = '3';
-				break;
-			case '3':
-				outputArray[i] = 'e';
-				break;
-			case 'l':
-				outputArray[i] = '1';
-				break;
-			case '1':
-				outputArray[i] = 'l';
-				break;
-			case 'o':
-				outputArray[i] = '0';
-				break;
-			case '0':
-				outputArray[i] = 'o';
-				break;
-			case 'a':
-				outputArray[i] = '4';
-				break;
-			case '4':
-				outputArray[i] = 'a';
-				break;
-			case 't':
-				outputArray[i] = '7';
-				break;
-			case '7':
-				outputArray[i] = 't';
-				break;
-			// sdfg
-			case 'b':
-				outputArray[i] = input.charAt(i);
-				break;
-			case 'c':
-				outputArray[i] = input.charAt(i);
-				break;
-			case 'd':
-				outputArray[i] = input.charAt(i);
-				break;
-			case 'f':
-				outputArray[i] = input.charAt(i);
-				break;
-			case 'g':
-				outputArray[i] = input.charAt(i);
-				break;
-			case 'h':
-				outputArray[i] = input.charAt(i);
-				break;
-			case 'i':
-				outputArray[i] = input.charAt(i);
-				break;
-			case 'j':
-				outputArray[i] = input.charAt(i);
-				break;
-			case 'k':
-				outputArray[i] = input.charAt(i);
-				break;
-			case 'm':
-				outputArray[i] = input.charAt(i);
-				break;
-			case 'n':
-				outputArray[i] = input.charAt(i);
-				break;
-			case 'p':
-				outputArray[i] = input.charAt(i);
-				break;
-			case 'q':
-				outputArray[i] = input.charAt(i);
-				break;
-			case 'r':
-				outputArray[i] = input.charAt(i);
-				break;
-			case 's':
-				outputArray[i] = input.charAt(i);
-				break;
-			case 'u':
-				outputArray[i] = input.charAt(i);
-				break;
-			case 'v':
-				outputArray[i] = input.charAt(i);
-				break;
-			case 'w':
-				outputArray[i] = input.charAt(i);
-				break;
-			case 'x':
-				outputArray[i] = input.charAt(i);
-				break;
-			case 'y':
-				outputArray[i] = input.charAt(i);
-				break;
-			case 'z':
-				outputArray[i] = input.charAt(i);
-				break;
-			case '2':
-				outputArray[i] = input.charAt(i);
-				break;
-			case '5':
-				outputArray[i] = input.charAt(i);
-				break;
-			case '6':
-				outputArray[i] = input.charAt(i);
-				break;
-			case '8':
-				outputArray[i] = input.charAt(i);
-				break;
-			case '9':
-				outputArray[i] = input.charAt(i);
-				break;
-
-			default:
-				break;
+		String output = "";
+		for (int x = 0; x < input.length(); x++) {
+			inner: for (int y = 0; y < arrayHin.length; y++) {
+				if (input.charAt(x) == arrayHin[y]) {
+					output = output + arrayZurueck[y];
+					break inner;
+				}
 			}
 		}
-		String outputString = new String(outputArray);
-		return outputString;
+		return output;
 	}
 
 	/**
@@ -146,127 +51,16 @@ public class CrypterImpl implements Crypter {
 	 *             (Großbuchstaben) enthält.
 	 */
 	public String decrypt(String input) throws IllegalArgumentException {
-		char[] outputArray = input.toCharArray();
-		for (int i = 0; i < input.length(); i++) {
-			char x = input.charAt(i);
-			switch (x) {
-			case 'e':
-				outputArray[i] = '3';
-				break;
-			case '3':
-				outputArray[i] = 'e';
-				break;
-			case 'l':
-				outputArray[i] = '1';
-				break;
-			case '1':
-				outputArray[i] = 'l';
-				break;
-			case 'o':
-				outputArray[i] = '0';
-				break;
-			case '0':
-				outputArray[i] = 'o';
-				break;
-			case 'a':
-				outputArray[i] = '4';
-				break;
-			case '4':
-				outputArray[i] = 'a';
-				break;
-			case 't':
-				outputArray[i] = '7';
-				break;
-			case '7':
-				outputArray[i] = 't';
-				break;
-			case 'b':
-				outputArray[i] = input.charAt(i);
-				break;
-			case 'c':
-				outputArray[i] = input.charAt(i);
-				break;
-			case 'd':
-				outputArray[i] = input.charAt(i);
-				break;
-			case 'f':
-				outputArray[i] = input.charAt(i);
-				break;
-			case 'g':
-				outputArray[i] = input.charAt(i);
-				break;
-			case 'h':
-				outputArray[i] = input.charAt(i);
-				break;
-			case 'i':
-				outputArray[i] = input.charAt(i);
-				break;
-			case 'j':
-				outputArray[i] = input.charAt(i);
-				break;
-			case 'k':
-				outputArray[i] = input.charAt(i);
-				break;
-			case 'm':
-				outputArray[i] = input.charAt(i);
-				break;
-			case 'n':
-				outputArray[i] = input.charAt(i);
-				break;
-			case 'p':
-				outputArray[i] = input.charAt(i);
-				break;
-			case 'q':
-				outputArray[i] = input.charAt(i);
-				break;
-			case 'r':
-				outputArray[i] = input.charAt(i);
-				break;
-			case 's':
-				outputArray[i] = input.charAt(i);
-				break;
-			case 'u':
-				outputArray[i] = input.charAt(i);
-				break;
-			case 'v':
-				outputArray[i] = input.charAt(i);
-				break;
-			case 'w':
-				outputArray[i] = input.charAt(i);
-				break;
-			case 'x':
-				outputArray[i] = input.charAt(i);
-				break;
-			case 'y':
-				outputArray[i] = input.charAt(i);
-				break;
-			case 'z':
-				outputArray[i] = input.charAt(i);
-				break;
-			case '2':
-				outputArray[i] = input.charAt(i);
-				break;
-			case '5':
-				outputArray[i] = input.charAt(i);
-				break;
-			case '6':
-				outputArray[i] = input.charAt(i);
-				break;
-			case '8':
-				outputArray[i] = input.charAt(i);
-				break;
-			case '9':
-				outputArray[i] = input.charAt(i);
-				break;
-
-			default:
-				throw new IllegalArgumentException("" + x);
-				//break;
+		for (int x = 0; x < input.length(); x++) {
+			inner: for (int y = 0; y < arrayHin.length; y++) {
+				if (input.charAt(x) == arrayHin[y]) {
+					break inner;
+				} else if (y == arrayHin.length - 1) {
+					throw new IllegalArgumentException();
+				}
 			}
 		}
-		String outputString = new String(outputArray);
-		return outputString;
+		return encrypt(input);
 
 	}
 }
-
