eb176fce2109fc7accfe5c9ce3dbc992e5b31f7e
b.collins <b.collins@bw-lp>
2016-12-05 14:50:09 +0100

    ich und santino haben alles fertig schöön

--- 10_collections/01_iterieren/src/tpe/collections/iterate/GameBoard.java
+++ 10_collections/01_iterieren/src/tpe/collections/iterate/GameBoard.java
@@ -3,7 +3,9 @@ package tpe.collections.iterate;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
+import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.List;
 
 import de.smits_net.games.framework.board.Board;
 import de.smits_net.games.framework.sprite.Sprite;
@@ -15,13 +17,14 @@ public class GameBoard extends Board {
 
     /** Anzahl der Münzen. */
     private static final int NUM_COINS = 1000;
-
+    List<Sprite> list1 = new ArrayList<Sprite>();
     /** Münzgenerator. */
     private CoinGenerator generator;
 
     /**
      * Erzeugt ein neues Board.
      */
+    
     public GameBoard() {
         // neues Spielfeld anlegen
         super(10, new Dimension(600, 600), Color.BLACK);
@@ -29,6 +32,14 @@ public class GameBoard extends Board {
         generator = new CoinGenerator(this, NUM_COINS);
 
         // TODO: Münzen generieren
+       Iterator<Sprite> iterSprite =generator.iterator();
+       
+       for(Sprite x:generator){
+           list1.add(x);
+       }
+//       while(iterSprite.hasNext()){
+//           list1.add(iterSprite.next());
+//       }
     }
 
 
@@ -38,6 +49,13 @@ public class GameBoard extends Board {
     @Override
     public synchronized void drawGame(Graphics g) {
         // TODO: Münzen zeichnen
+        Iterator<Sprite> x = list1.iterator();
+//        while(x.hasNext()){
+//            x.next().draw(g);
+//        }
+        for(Sprite y:list1){
+           y.draw(g);
+        }
     }
 
     /**
